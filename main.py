1d072f7b6ca476bfb91f8148d1ea0afc1b06b862
Tasos <vlahbeis.tasos@gmail.com>
2026-07-28 19:53:09 +0300

    fin

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import math as m
-# A multi-power function solver
-# returns roots for function of type Xx^2 + Yx + Z
-def solver(params):
+# A quadratic function solver
+# returns roots
+
+def qSolver(params):
     #l = len(params)
     a = params[0]
     b = params[1]
     c = params[2]
-    r1 = (-b + m.sqrt(b^2 - 4*a*c)) / 2*a
-    r2 = (-b - m.sqrt(b^2 - 4*a*c)) / 2*a
-    return [r1,r2]
+    disc = b**2 - 4*a*c
+
+    if disc == 0:
+        return [-b/(2*a)]
+    elif disc > 0:
+        return [(-b + m.sqrt(disc)) / (2*a),(-b - m.sqrt(disc)) / (2*a)]
+    else:
+        return []
 
 x = 0
-
 params = []
+
 for _ in range(3):
     x = int(input("num of curr: "))
     params.append(x)
 
-roots = solver(params)
-
+roots = qSolver(params)
 print(roots)
